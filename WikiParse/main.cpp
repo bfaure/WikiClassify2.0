4825dbca9e3f90bbe7fbf01a9be23e5e86b3cb2b
Brian Faure <bfaure23@gmail.com>
2017-01-29 03:23:33 -0500

    Email is now sent by default

--- WikiParse/main.cpp
+++ WikiParse/main.cpp
@@ -6,7 +6,7 @@ string email_to = "bfaure23@gmail.com"; // change this to your email
 int main(int argc, const char * argv[]) {
 	if (argc == 2) {
         wikidump dump(argv[1]); // Specify where the dump is, given first argument
-        dump.read(); 
+        dump.read(email_to); 
         return 0;
 	}
 	else
