cb54929c1d8391e2219323c6919f86b01b0f313a
Brian Faure <bfaure23@gmail.com>
2017-01-30 18:37:29 -0500

    Changed examiner.py target file to documents.json

--- wikiparse/examiner.py
+++ wikiparse/examiner.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 import json, codecs, time
 from datetime import datetime
-
 from operator import attrgetter
 
 
@@ -10,13 +9,13 @@ class url_t:
         self.url = url 
         self.ct = 0
 
-filename = "documents_simple.json"
+filename = "documents.json"
 
 start_time = time.time()
 print("Loading...",end="\r")
 with open(filename,"r") as data_file:
     dump = json.load(data_file,"ISO-8859-1")
-print("Loading... done, "+str(time.time()-start_time)+" seconds")
+print("Loading... done, "+str(time.time()-start_time)[:5]+" seconds")
 
 urls = []
 
@@ -28,20 +27,21 @@ for article in dump:
         found = False
         for found_url in urls:
             if found_url.url == url:
-                found_url.ct = found_url.ct+int(dump[article]['size'])
+                found_url.ct = found_url.ct+(float(dump[article]['size'])/1000000)
                 found = True
                 break
         if found==False:
             new_url = url_t(url)
-            new_url.ct = int(dump[article]['size'])
+            new_url.ct = (float(dump[article]['size'])/1000000)
             urls.append(new_url)
 
-
-print("Reading documents... done, "+str(time.time()-start_time)+" seconds")
+print("Reading documents... done, "+str(time.time()-start_time)[:5]+" seconds")
 
 sorted_urls = sorted(urls, key=attrgetter('ct'), reverse=True)
 
-for i in range(10):
+print("\n")
+
+for i in range(25):
     print(sorted_urls[i].url+" "+str(sorted_urls[i].ct))
 
 
