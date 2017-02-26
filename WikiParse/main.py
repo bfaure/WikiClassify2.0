7b46e3f5708ee6f2de97d94940a2cf70722d24fa
Nathan Kjer <nathan.kjer@rutgers.edu>
2017-02-26 03:27:40 -0500

    Now word2vec for categories
    Main file for wikiparse now needs heavy work

--- WikiParse/main.py
+++ WikiParse/main.py
@@ -27,8 +27,199 @@ from gensim.corpora.mmcorpus   import MmCorpus
 
 from sklearn.preprocessing import MultiLabelBinarizer
 
+#                          Corpus wrapper class
+#-----------------------------------------------------------------------------#
+
+class wiki_corpus(object):
+
+    def __init__(self, corpus_name, corpus_directory):
+        print("Initializing '%s' corpus..." % corpus_name)
+
+        self.name = corpus_name
+
+        # Standard directories
+        self.meta_directory = corpus_directory+'/meta'
+        self.data_directory = corpus_directory+'/data'
+
+        prev_date = '20010115'
+        for date, dump_url in zip(self.get_dump_dates(),self.get_dump_urls()):
+            date_directory = self.data_directory+'/'+date
+            dump_path = download(dump_url, date_directory)
+            dump_path = expand_bz2(dump_path)
+            if not os.path.isfile(date_directory+'/article_revision_text.txt'):
+                self.parse(dump_path, prev_date)
+            prev_date = date
+
+            #category_revs  = self.data_directory+'/'+date+'/category_revisions.txt'
+            #category_names = self.data_directory+'/'+date+'/category_names.txt'
+            #category_tree  = self.data_directory+'/'+date+'/category_revision_parents.txt'
+
+            # Create iterators from specialized classes
+            self.revision_categories = category_corpus(date_directory+'/article_revision_categories.txt')
+            self.category_names = build_mapping(date_directory+'/category_names.txt').values()
+            break
+
+    def get_dump_dates(self):
+        url  = 'https://dumps.wikimedia.org/%s/' % self.name
+        tree = html.fromstring(requests.get(url).text)
+        return [x[:-1] for x in sorted(tree.xpath('//a/@href'))[1:-1]]
+
+    def get_dump_urls(self):
+        dump_urls = []
+        url  = 'https://dumps.wikimedia.org/%s/' % self.name
+        for date in self.get_dump_dates():
+            if 'in progress' not in requests.get(url+date).text:
+                dump_url = '{0}{1}/{2}-{1}-pages-meta-current.xml.bz2'.format(url,date,self.name)
+                dump_urls.append(dump_url)
+        return dump_urls
+
+    def parse(self, dump_path, prev_date):
+        compiled = self.compile_parser()
+        if not compiled:
+            print("\tCould not compile parser!")
+            return
+        else:
+            self.run_parser(dump_path, os.path.dirname(dump_path), prev_date)
+
+    def compile_parser(self, recompile=False):
+        print("\tCompiling parser...")
+        if recompile and os.path.isfile('wikiparse.out'):
+            os.remove('wikiparse.out')
+        if not os.path.isfile('wikiparse.out'):
+            try:
+                call(["g++","--std=c++11","-O3"]+["WikiParse/code/"+x for x in ["main.cpp","wikidump.cpp","wikipage.cpp","wikitext.cpp","string_utils.cpp"]]+["-o","wikiparse.out"])
+            except:
+                return False
+        else:
+            print("\t\tParser already compiled.")
+        return True
+
+    def run_parser(self, dump_path, output_directory, prev_date):
+        print("\tRunning parser...")
+        call(["./wikiparse.out", dump_path, output_directory, prev_date])
+
+#                      Tagged Document iterator
+#-----------------------------------------------------------------------------#
+
+class doc_corpus(object):
+
+    def __init__(self, document_path):
+        self.document_path  = document_path
+
+    def __iter__(self):
+        for i, doc in self.indexed_docs():
+            tokens = [x for x in doc if x in self.get_words()]
+            yield TaggedDocument(self.trigram[self.bigram[tokens]],[i])
+
+    def process(self, doc):
+        for i, doc in self.indexed_docs():
+            tokens = [x for x in doc if x in self.get_words()]
+            yield self.trigram[self.bigram[tokens]]
+
+    def docs_phrased(self):
+        for tokens in self.docs():
+            self.trigram[self.bigram[tokens]]
+
+    def docs(self):
+        for _, doc in self.indexed_docs():
+            yield [x for x in doc]
+
+    def indexed_docs(self):
+        with open(self.document_path) as fin:
+            for line in fin:
+                values = line[:-1].split('\t')
+                if len(values) > 1:
+                    yield values[0], values[1:]
+
+    # Phrase methods
+
+    def save(self, directory):
+        print("\tSaving gram detector...")
+        check_directory(directory)
+        self.bigram.save(directory+'/bigrams.pkl')
+        self.trigram.save(directory+'/trigrams.pkl')
+
+        print("\tSaving dictionary...")
+        self.dictionary.save(directory+'/dictionary.dict')
+        self.dictionary.save_as_text(directory+'/word_list.tsv')
+
+    def load(self, directory):
+        print("\tLoading gram detector...")
+        self.bigram = Dictionary.load(directory+'/bigrams.pkl')
+        self.trigram = Dictionary.load(directory+'/trigrams.pkl')
+
+        print("\tLoading dictionary...")
+        self.dictionary = Dictionary.load(directory+'/dictionary.dict')
+
+    # Dictionary methods
+
+    def train(self):
+
+        print("\t\tTraining bigram detector...")
+        self.bigram = Phraser(Phrases(self.docs(), min_count=5, threshold=10, max_vocab_size=100000))
+        print("\t\tTraining trigram detector...")
+        self.trigram = Phraser(Phrases(self.bigram[self.docs.docs()], min_count=5, threshold=10, max_vocab_size=100000))
+
+        print("\tBuilding dictionary...")
+        self.dictionary = Dictionary(self.docs_phrased(), prune_at=2000000)
+        self.dictionary.filter_extremes(no_below=3, no_above=0.5, keep_n=100000)
+
+    def get_word_map(self):
+        print("\tGetting word map...")
+        return dict((v,k) for k,v in self.dictionary.token2id.iteritems())
+
+    def get_words(self):
+        return self.dictionary.values()
+
+#    # Category methods
+#
+#    def get_category_names(self):
+#        return [self.category_map[i] for i in sorted(self.category_map.keys())]
+#
+#    def get_doc_categories(self, limit=-1):
+#        print("\tLoading classes...")
+#        categories = []
+#        for i,category in enumerate(self.docs.categories()):
+#            categories.append(category)
+#            if i == limit:
+#                break
+#        mlb = MultiLabelBinarizer(classes=self.category_map.keys())
+#        return mlb.fit_transform(np.array(categories))
+
+
+class category_corpus(object):
+
+    def __init__(self, document_path):
+        self.document_path  = document_path
+        self.instances = sum(1 for line in open(self.document_path))
+
+    def __iter__(self):
+        for i, doc in self.indexed_docs():
+            yield TaggedDocument(doc,[i])
+
+    def docs(self):
+        for _, doc in self.indexed_docs():
+            yield doc
+
+    def indexed_docs(self):
+        with open(self.document_path) as fin:
+            for line in fin:
+                values = line[:-1].split('\t')
+                if len(values) > 1:
+                    yield values[0], values[1:]
+
 #                             Global functions
 #-----------------------------------------------------------------------------#
+
+def build_mapping(tsv_path):
+    mapping = {}
+    with open(tsv_path) as f:
+        for line in f:
+            values = line[:-1].split('\t')
+            if len(values) == 2:
+                key, value = values
+                mapping[key] = value
+    return mapping
 
 def tokenize(text):
     return re.split('\W+', utils.to_unicode(text).lower())
@@ -93,196 +284,3 @@ def download(url, directory):
     else:
         print("\t\t\t'%s' already exists." % file_name)
     return file_path
-
-#                          Corpus wrapper class
-#-----------------------------------------------------------------------------#
-
-class wiki_corpus(object):
-
-    def __init__(self, corpus_name, corpus_directory):
-        print("Initializing '%s' corpus..." % corpus_name)
-
-        self.name = corpus_name
-
-        # Standard directories
-        self.meta_directory = corpus_directory+'/meta'
-        self.data_directory = corpus_directory+'/data'
-
-        prev_date = '20010115'
-        for date, dump_url in zip(self.get_dump_dates(),self.get_dump_urls()):
-            dump_path = download(dump_url, self.data_directory+'/'+date)
-            dump_path = expand_bz2(dump_path)
-            self.parse(dump_path, prev_date)
-            prev_date = date
-
-#        # Interpret categories
-#        self.load_category_map()
-#        self.load_category_tree()
-#
-#        # Create iterators from specialized classes
-#        self.docs = doc_corpus(self)
-#        self.bags = bag_corpus(self)
-#
-#        # Obtain preprocessor
-#        if not os.path.exists(self.meta_directory):
-#            self.train_phrases()
-#            self.train_dictionary()
-#            self.save_phrases()
-#            self.save_dictionary()
-#        else:
-#            self.load_phrases()
-#            self.load_dictionary()
-
-    def get_dump_dates(self):
-        url  = 'https://dumps.wikimedia.org/%s/' % self.name
-        tree = html.fromstring(requests.get(url).text)
-        return [x[:-1] for x in sorted(tree.xpath('//a/@href'))[1:-1]]
-
-    def get_dump_urls(self):
-        dump_urls = []
-        url  = 'https://dumps.wikimedia.org/%s/' % self.name
-        for date in self.get_dump_dates():
-            if 'in progress' not in requests.get(url+date).text:
-                dump_url = '{0}{1}/{2}-{1}-pages-meta-current.xml.bz2'.format(url,date,self.name)
-                dump_urls.append(dump_url)
-        return dump_urls
-
-    def parse(self, dump_path, prev_date):
-        compiled = self.compile_parser()
-        if not compiled:
-            print("\tCould not compile parser!")
-            return
-        else:
-            self.run_parser(dump_path, os.path.dirname(dump_path), prev_date)
-
-    def compile_parser(self, recompile=False):
-        print("\tCompiling parser...")
-        if recompile and os.path.isfile('wikiparse.out'):
-            os.remove('wikiparse.out')
-        if not os.path.isfile('wikiparse.out'):
-            try:
-                call(["g++","--std=c++11","-O3"]+["WikiParse/code/"+x for x in ["main.cpp","wikidump.cpp","wikipage.cpp","wikitext.cpp","string_utils.cpp"]]+["-o","wikiparse.out"])
-            except:
-                return False
-        else:
-            print("\t\tParser already compiled.")
-        return True
-
-    def run_parser(self, dump_path, output_directory, prev_date):
-        print("\tRunning parser...")
-        call(["./wikiparse.out", dump_path, output_directory, prev_date])
-
-    def process(self, text):
-        tokens = [x for x in tokenize(text) if x in self.get_words()]
-        return self.trigram[self.bigram[tokens]]
-
-    # Category methods
-
-    def load_category_map(self):
-        self.category_map = {}
-        with open(self.category_map_path) as fin:
-            for line in fin:
-                c_code, name = line.split('\t')
-                self.category_map[int(c_code)] = name.strip()
-
-    def load_category_tree(self):
-        self.category_tree = {}
-        with open(self.category_tree_path) as fin:
-            for line in fin:
-                child, parent = line.split('\t')
-                parent = [int(x.strip()) for x in parent.split(',')]
-                self.category_tree[int(child)] = parent
-
-    def get_category_names(self):
-        return [self.category_map[i] for i in sorted(self.category_map.keys())]
-
-    def get_doc_categories(self, limit=-1):
-        print("\tLoading classes...")
-        categories = []
-        for i,category in enumerate(self.docs.categories()):
-            categories.append(category)
-            if i == limit:
-                break
-        mlb = MultiLabelBinarizer(classes=self.category_map.keys())
-        return mlb.fit_transform(np.array(categories))
-
-    # Dictionary methods
-
-    def get_word_map(self):
-        print("\tGetting word map...")
-        return dict((v,k) for k,v in self.dictionary.token2id.iteritems())
-
-    def get_words(self):
-        return self.dictionary.values()
-
-    def train_dictionary(self):
-        print("\tBuilding dictionary...")
-        self.dictionary = Dictionary(self.docs.untagged(), prune_at=2000000)
-        self.dictionary.filter_extremes(no_below=3, no_above=0.5, keep_n=100000)
-
-    def save_dictionary(self):
-        print("\tSaving dictionary...")
-        check_directory(self.meta_directory)
-        self.dictionary.save(self.meta_directory+'/dictionary.dict')
-        self.dictionary.save_as_text(self.meta_directory+'/word_list.tsv')
-
-    def load_dictionary(self):
-        print("\tLoading dictionary...")
-        self.dictionary = Dictionary.load(self.meta_directory+'/dictionary.dict')
-
-    # Phrase methods
-
-    def train_phrases(self):
-        print("\tTraining phrases...")
-        print("\t\tTraining bigram detector...")
-        self.bigram = Phraser(Phrases(self.docs.docs(), min_count=5, threshold=10, max_vocab_size=100000))
-        print("\t\tTraining trigram detector...")
-        self.trigram = Phraser(Phrases(self.bigram[self.docs.docs()], min_count=5, threshold=10, max_vocab_size=100000))
-
-    def save_phrases(self):
-        print("\tSaving gram detector...")
-        check_directory(self.meta_directory)
-        self.bigram.save(self.meta_directory+'/bigrams.pkl')
-        self.trigram.save(self.meta_directory+'/trigrams.pkl')
-
-    def load_phrases(self):
-        print("\tLoading gram detector...")
-        self.bigram = Dictionary.load(self.meta_directory+'/bigrams.pkl')
-        self.trigram = Dictionary.load(self.meta_directory+'/trigrams.pkl')
-
-#                             Bag iterator
-#-----------------------------------------------------------------------------#
-
-#class bag_corpus(object):
-#    def __init__(self, corpus):
-#        self.corpus = corpus
-#    def __iter__(self):
-#        for doc in self.corpus.docs.untagged():
-#            yield self.corpus.dictionary.doc2bow(doc)
-#
-##                      Tagged Document iterator
-##-----------------------------------------------------------------------------#
-#
-#class doc_corpus(object):
-#    def __init__(self, corpus):
-#        self.corpus = corpus
-#        print('\tCounting instances...')
-#        with open(self.corpus.document_path, 'rb') as fin:
-#            self.instances = sum(1 for doc in fin)
-#
-#    def __iter__(self):
-#        for i,doc in enumerate(self.docs()):
-#            yield TaggedDocument(self.corpus.trigram[self.corpus.bigram[doc]],[i])
-#    def untagged(self):
-#        for doc in self.docs():
-#            yield self.corpus.trigram[self.corpus.bigram[doc]]
-#    def docs(self):
-#        with open(self.corpus.document_path, 'rb') as fin:
-#            for doc in fin:
-#                categories, doc = doc.split('\t')
-#                yield doc.split()
-#    def categories(self):
-#        with open(self.corpus.document_path, 'rb') as fin:
-#            for doc in fin:
-#                categories, doc = doc.split('\t')
-#                yield [int(x.strip()) for x in categories.split(',')]
--- main.py
+++ main.py
@@ -17,19 +17,26 @@ from WikiLearn.code.vectorize import LDA, doc2vec
 
 corpus_name   = 'simplewiki'
 run_LDA       = False
-run_word2vec  = False
+run_word2vec  = True
 corpus_directory   = 'WikiParse/data/corpora/%s' % corpus_name
 LDA_directory      = 'WikiLearn/data/models/LDA/%s' % corpus_name
 word2vec_directory = 'WikiLearn/data/models/word2vec/%s' % corpus_name
-
 def main():
     documents = wiki_corpus(corpus_name, corpus_directory)
     if run_LDA:
         start_time = time.time()
-        encoder = LDA(documents, LDA_directory)
+        encoder = LDA(documents.categories, LDA_directory)
     if run_word2vec:
         start_time = time.time()
-        encoder = doc2vec(documents, word2vec_directory)
+        encoder = doc2vec(documents.revision_categories, word2vec_directory)
+        for doc in documents.category_names:
+            catname = doc[9:].replace('_',' ')
+            if catname.strip():
+                nearest = encoder.get_nearest_word(doc[9:])
+                if nearest:
+                    print('\t'.join(nearest))
+            else:
+                print("Nope")
 
 if __name__ == "__main__":
     main()
