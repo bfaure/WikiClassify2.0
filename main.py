7e862256fa61fcadd47e870a0287f184a0f14eb9
Nathan Kjer <nathan.kjer@rutgers.edu>
2017-03-02 18:16:28 -0500

    Streamlined related token API

--- main.py
+++ main.py
@@ -22,39 +22,21 @@ corpus_directory   = 'WikiParse/data/corpora/%s' % corpus_name
 LDA_directory      = 'WikiLearn/data/models/LDA/%s' % corpus_name
 word2vec_directory = 'WikiLearn/data/models/doc2vec/%s' % corpus_name
 
+def save_related_tokens(doc_iter, model_name):
+    encoder = doc2vec(doc_iter, word2vec_directory+'/'+model_name)
+    with open('related_%s.tsv' % model_name,'w+') as f:
+        for word in encoder.model.index2word:
+            nearest = encoder.get_nearest_word(word)
+            if nearest:
+                nearest = [x for x in nearest if x != word]
+                f.write(word+'\t'+'\t'.join(nearest)+'\n')
+
 def main():
     documents = wiki_corpus(corpus_name, corpus_directory)
-    if run_LDA:
-        encoder = LDA(documents.categories, LDA_directory)
-    if run_word2vec:
-        encoder = doc2vec(documents.get_revision_text(), word2vec_directory+'/text')
-        with open('related_words.tsv','w+') as f:
-            for word in encoder.model.index2word:
-                nearest = encoder.get_nearest_word(word)
-                if nearest:
-                    nearest = [x for x in nearest if x != word]
-                    f.write(word+'\t'+'\t'.join(nearest)+'\n')
-#        encoder = doc2vec(documents.get_revision_categories(), word2vec_directory+'/categories')
-#        with open('related_categories.tsv','w+') as f:
-#            for word in encoder.model.index2word:
-#                nearest = encoder.get_nearest_word(word)
-#                if nearest:
-#                    nearest = [x for x in nearest if x != word]
-#                    f.write(word+'\t'+'\t'.join(nearest)+'\n')
-#        encoder = doc2vec(documents.get_revision_cited_authors(), word2vec_directory+'/cited_authors')
-#        with open('related_authors.tsv','w+') as f:
-#            for word in encoder.model.index2word:
-#                nearest = encoder.get_nearest_word(word)
-#                if nearest:
-#                    nearest = [x for x in nearest if x != word]
-#                    f.write(word+'\t'+'\t'.join(nearest)+'\n')
-#        encoder = doc2vec(documents.get_revision_cited_domains(), word2vec_directory+'/cited_domains')
-#        with open('related_domains.tsv','w+') as f:
-#            for word in encoder.model.index2word:
-#                nearest = encoder.get_nearest_word(word)
-#                if nearest:
-#                    nearest = [x for x in nearest if x != word]
-#                    f.write(word+'\t'+'\t'.join(nearest)+'\n')
+    save_related_tokens(doc_iter=documents.get_revision_words(), model_name='words')
+    save_related_tokens(doc_iter=documents.get_revision_categories(), model_name='categories')
+    save_related_tokens(doc_iter=documents.get_revision_cited_authors(), model_name='cited_authors')
+    save_related_tokens(doc_iter=documents.get_revision_cited_domains(), model_name='cited_domains')
 
 if __name__ == "__main__":
     main()
