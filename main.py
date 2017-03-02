c3a01c4d1298908f73bfa0787428d65c7f34f721
Nathan Kjer <nathan.kjer@rutgers.edu>
2017-03-01 20:01:10 -0500

    Now works with ML

--- main.py
+++ main.py
@@ -23,18 +23,18 @@ LDA_directory      = 'WikiLearn/data/models/LDA/%s' % corpus_name
 word2vec_directory = 'WikiLearn/data/models/word2vec/%s' % corpus_name
 def main():
     documents = wiki_corpus(corpus_name, corpus_directory)
-    #if run_LDA:
-    #    start_time = time.time()
-    #    encoder = LDA(documents.categories, LDA_directory)
-    #if run_word2vec:
-    #    start_time = time.time()
-    #    encoder = doc2vec(documents.get_revision_categories(), word2vec_directory)
-    #    with open('related_categories.tsv','w+') as f:
-    #        for category_name in documents.get_category_names():
-    #            nearest = encoder.get_nearest_word(category_name)
-    #            if nearest:
-    #                nearest = [x for x in nearest if x != category_name]
-    #                f.write(category_name+'\t'+'\t'.join(nearest)+'\n')
+    if run_LDA:
+        start_time = time.time()
+        encoder = LDA(documents.categories, LDA_directory)
+    if run_word2vec:
+        start_time = time.time()
+        encoder = doc2vec(documents.get_revision_categories(), word2vec_directory)
+        with open('related_categories.tsv','w+') as f:
+            for category_name in documents.get_category_names():
+                nearest = encoder.get_nearest_word(category_name)
+                if nearest:
+                    nearest = [x for x in nearest if x != category_name]
+                    f.write(category_name+'\t'+'\t'.join(nearest)+'\n')
 
 if __name__ == "__main__":
     main()
