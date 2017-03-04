765b4c4c0cbdaf8758c927cf57c54da1c586e8ed
Nathan Kjer <nathan.kjer@rutgers.edu>
2017-03-04 01:05:30 -0500

    Better with expanded vocabulary

--- main.py
+++ main.py
@@ -25,41 +25,41 @@ word2vec_directory = 'WikiLearn/data/models/doc2vec/%s' % corpus_name
 def save_related(model_name):
     documents = wiki_corpus(corpus_name, corpus_directory)
 
-#    doc_iter  = documents.get_doc_iter(model_name)
-#    encoder = doc2vec(doc_iter, word2vec_directory+'/'+model_name)
-#    if not os.path.exists(word2vec_directory+'/'+model_name):
-#        if model_name == 'words':
-#            encoder.build(features=400, context_window=10, min_count=5, sample=1e-5, negative=20)
-#            encoder.train(epochs=10)
-#        elif model_name == 'categories':
-#            encoder.build(features=200, context_window=50, min_count=3, sample=1e-5, negative=5)
-#            encoder.train(epochs=50)
-#        elif model_name == 'cited_authors':
-#            encoder.build(features=20, context_window=50, min_count=2)
-#            encoder.train(epochs=100)
-#        elif model_name == 'cited_domains':
-#            encoder.build(features=80, context_window=50, min_count=2)
-#            encoder.train(epochs=100)
-#        encoder.save()
-#    else:
-#        self.load()
-#
-#    print('Backtesting...')
-#    with open('related_%s_tokens.tsv' % model_name,'w+') as f:
-#        for word in encoder.model.index2word:
-#            nearest = encoder.get_nearest_word(word)
-#            if nearest:
-#                nearest = [x for x in nearest if x != word]
-#                f.write(word+'\t'+'\t'.join(nearest)+'\n')
-#
-#    print('Backtesting...')
-#    with open('related_%s_docs.tsv' % model_name,'w+') as f:
-#        for doc_id in encoder.model.docvecs.offset2doctag:
-#            nearest = encoder.get_nearest_doc(doc_id)
-#            f.write(doc_id+'\t'+'\t'.join(nearest)+'\n')
+    doc_iter  = documents.get_doc_iter(model_name)
+    encoder = doc2vec(doc_iter, word2vec_directory+'/'+model_name)
+    if not os.path.exists(word2vec_directory+'/'+model_name):
+        if model_name == 'words':
+            encoder.build(features=400, context_window=10, min_count=5, sample=1e-5, negative=20)
+            encoder.train(epochs=10)
+        elif model_name == 'categories':
+            encoder.build(features=200, context_window=50, min_count=1, sample=1e-5, negative=5)
+            encoder.train(epochs=50)
+        elif model_name == 'cited_authors':
+            encoder.build(features=20, context_window=50, min_count=1)
+            encoder.train(epochs=100)
+        elif model_name == 'cited_domains':
+            encoder.build(features=80, context_window=50, min_count=1)
+            encoder.train(epochs=100)
+        encoder.save()
+    else:
+        self.load()
+
+    print('Backtesting...')
+    with open('related_%s_tokens.tsv' % model_name,'w+') as f:
+        for word in encoder.model.index2word:
+            nearest = encoder.get_nearest_word(word)
+            if nearest:
+                nearest = [x for x in nearest if x != word]
+                f.write(word+'\t'+'\t'.join(nearest)+'\n')
+
+    print('Backtesting...')
+    with open('related_%s_docs.tsv' % model_name,'w+') as f:
+        for doc_id in encoder.model.docvecs.offset2doctag:
+            nearest = encoder.get_nearest_doc(doc_id)
+            f.write(doc_id+'\t'+'\t'.join(nearest)+'\n')
 
     revision_map = documents.get_revision_titles()
-    with open('related_%s_docs.tsv' % model_name) as f:
+    with open('related_%s_docs.tsv' % model_name, 'w+') as f:
         for line in f:
             related = []
             for doc_id in line.strip().split('\t'):
