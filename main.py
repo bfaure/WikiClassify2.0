caa2a3eee84937476ae05da253898b8032973664
Nathan Kjer <nathan.kjer@rutgers.edu>
2017-03-06 19:10:11 -0500

    Fixing related categories

--- main.py
+++ main.py
@@ -20,38 +20,38 @@ def save_related():
     encoder_directory = 'WikiLearn/data/models/tokenizer'
 
     encoder = get_encoder('text.tsv',True,encoder_directory+'/text',400,10,5,20,10)
-    save_related_tokens(encoder, 'related_authors.tsv')
-    save_related_docs(encoder, 'related_docs_authors.tsv')
+    save_related_tokens(encoder, 'related_text.tsv')
+    save_related_docs(encoder, 'related_docs_text.tsv')
 
     encoder = get_encoder('categories.tsv',False,encoder_directory+'/categories',200,50,1,5,20)
-    save_related_tokens(encoder, 'related_authors.tsv')
-    save_related_docs(encoder, 'related_docs_authors.tsv')
+    save_related_tokens(encoder, 'related_categories.tsv')
+    save_related_docs(encoder, 'related_docs_categories.tsv')
 
     encoder = get_encoder('links.tsv',False,encoder_directory+'/links',200,50,2,5,20)
-    save_related_tokens(encoder, 'related_authors.tsv')
-    save_related_docs(encoder, 'related_docs_authors.tsv')
+    save_related_tokens(encoder, 'related_links.tsv')
+    save_related_docs(encoder, 'related_docs_links.tsv')
 
     encoder = get_encoder('authors.tsv',False,encoder_directory+'/authors',20,50,1,1,50)
     save_related_tokens(encoder, 'related_authors.tsv')
     save_related_docs(encoder, 'related_docs_authors.tsv')
 
     encoder = get_encoder('domains.tsv',False,encoder_directory+'/domains',80,50,1,1,50)
-    save_related_tokens(encoder, 'related_authors.tsv')
-    save_related_docs(encoder, 'related_docs_authors.tsv')
+    save_related_tokens(encoder, 'related_domains.tsv')
+    save_related_docs(encoder, 'related_docs_domains.tsv')
 
 def save_related_tokens(encoder, path):
     print('Saving related tokens...')
     with open(path,'w+') as f:
         for word in encoder.model.index2word:
             nearest = encoder.get_nearest_word(word)
-            f.write(word+'\t\t'.join(nearest)+'\n')
+            f.write(word+'\t'+'\t'.join(nearest)+'\n')
 
 def save_related_docs(encoder, path):
     print('Saving related docs...')
     with open(path,'w+') as f:
         for doc_id in encoder.model.docvecs.offset2doctag:
             nearest = encoder.get_nearest_doc(doc_id)
-            f.write(doc_id+'\t\t'.join(nearest)+'\n')
+            f.write(doc_id+'\t'+'\t'.join(nearest)+'\n')
 
 def get_encoder(tsv_path, make_phrases, directory, features, context_window, min_count, negative, epochs):
     documents = gensim_corpus(tsv_path,directory,make_phrases)
