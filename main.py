1bddc99c88b5cd2198e59c37de1f341e12a25e05
Brian Faure <bfaure23@gmail.com>
2017-03-09 16:20:42 -0500

    added 3c*

--- main.py
+++ main.py
@@ -170,6 +170,146 @@ def rectify_path(path_end,dictionary=None):
 def string_compare(str1,str2):
 	return SequenceMatcher(None,str1,str2).ratio()
 
+def astar_convene_3(start_query,middle_query,end_query,encoder,weight=4.0,branching_factor=10.0,dictionary=None):
+	if dictionary is not None:
+		saved_start_query = start_query 
+		saved_end_query = end_query 
+		start_key = None 
+		end_key = None
+
+		try:
+			start_key = next(key for key,value in dictionary.items() if value==start_query)
+		except:
+			try:
+				start_key = next(key for key,value in dictionary.items() if value.lower()==start_query.lower())
+			except:
+				print("Could not find dictionary id for "+start_query)
+				for key,value in dictionary.items():
+					#if abs(len(value)-len(saved_start_query))>2: continue
+					if value.lower()[:9]!=start_query.lower()[:9]: continue
+					if string_compare(value.lower()[9:],start_query.lower()[9:])>=0.7:
+						wants_this = raw_input("Did you mean \""+value+"\"? [Y,n,restart]: ")
+						if wants_this in ["y","Y","yes","Yes",""]:
+							start_key = key 
+							saved_start_query = value 
+							break
+						if wants_this in ["r","restart"]:
+							print("Canceling search.")
+							return
+
+		if start_key==None: 
+			print("Canceling search, could not locate "+start_query)
+			return -1
+
+		try:
+			end_key = next(key for key,value in dictionary.items() if value==end_query)
+		except:
+			try:
+				end_key = next(key for key,value in dictionary.items() if value.lower()==end_query.lower())
+			except:
+				print("Could not find dictionary id for "+end_query)
+				for key,value in dictionary.items():
+					if value.lower()[:9]!=start_query.lower()[:9]: continue
+					#if abs(len(value)-len(saved_end_query))>2: continue
+					if string_compare(value.lower()[9:],end_query.lower()[9:])>=0.7:
+						wants_this = raw_input("Did you mean \""+value+"\"? [Y,n,restart]: ")
+						if wants_this in ["y","Y","yes","Yes",""]:
+							end_key = key 
+							saved_end_query = value 
+							break
+						if wants_this in ["r","restart"]:
+							print("Canceling search.")
+							return
+
+		if start_key==None or end_key==None: return -1
+
+		start_query = start_key 
+		end_query = end_key
+
+	start_vector = encoder.get_nearest_word(start_query,topn=branching_factor)
+	end_vector = encoder.get_nearest_word(end_query,topn=branching_factor)
+
+	if start_vector==None:  print("Could not find relation vector for "+start_query)
+	if end_vector==None:    print("Could not find relation vector for "+end_query)
+	if start_vector==None or end_vector==None: return -1
+
+	start_elem = elem_t(start_query,parent=None,cost=0)
+	
+	frontier = PriorityQueue()
+	start_elem_cost = get_transition_cost(start_query,end_query,encoder)
+	start_elem.cost = start_elem_cost
+	frontier.push(start_elem)
+
+	cost_list = {}
+	cost_list[start_query] = 0
+
+	path_end = start_elem 
+	base_cost = 0
+	explored = []
+	search_start = time()
+	return_code = "NONE"
+
+	middle_word = "NONE"
+	dist_middle_start = 1000
+	dist_middle_end = 1000
+	dist_middle_middle = 1000
+
+	while True:
+		print("explored: "+str(len(explored))+", frontier: "+str(frontier.length())+", time: "+str(time()-search_start)[:6]+", cost: "+str(base_cost)[:5],end="\r")
+		sys.stdout.flush()
+
+		if frontier.length()==0:
+			print("\nA* Convene failed.")
+			return_code = "NOT FOUND"
+			break
+
+		cur_node = frontier.pop()
+		cur_word = cur_node.value
+		explored.append(cur_word)
+
+		if cur_word not in [start_query,end_query,middle_query]:
+			dist_to_middle = get_transition_cost(cur_word,middle_query,encoder)
+			dist_to_start = get_transition_cost(cur_word,start_query,encoder)
+			dist_to_end = get_transition_cost(cur_word,end_query,encoder)
+			if dist_to_end<dist_middle_start and dist_to_start<dist_middle_end and dist_to_middle<dist_middle_middle:
+				middle_word = cur_word 
+				dist_middle_start = dist_to_start
+				dist_middle_end = dist_to_end
+				dist_middle_middle = dist_to_middle
+
+		if cur_word==end_query:
+			print("\nFound connection.")
+			path_end = cur_node 
+			break
+
+		neighbors = encoder.get_nearest_word(cur_word,topn=branching_factor)
+		if neighbors==None: continue
+		base_cost = cost_list[cur_word]
+
+		for neighbor_word in neighbors:
+			if cur_word==neighbor_word: continue
+			cost = base_cost + get_transition_cost(cur_word,neighbor_word,encoder)
+
+			new_elem = elem_t(neighbor_word,parent=cur_node,cost=cost)
+			new_elem.column_offset = neighbors.index(neighbor_word)
+
+			if (neighbor_word not in cost_list or cost<cost_list[neighbor_word]) and neighbor_word not in explored:
+				cost_list[neighbor_word] = cost 
+				new_elem.cost = cost + (float(weight)*(get_transition_cost(neighbor_word,end_query,encoder))) + (float(weight))*get_transition_cost(neighbor_word,middle_query,encoder)
+				frontier.push(new_elem)
+
+	if middle_word=="None":
+		print("Words are too similar to be compared, try lower weight & higher branching factor.")
+		return
+
+	print("                                                                \r",end="\r")
+	print("\n=========================================")
+	print(start_query+" + "+middle_query+" + "+end_query+" = "+middle_word+"\n")
+	print(middle_word+" --> "+start_query+" similarity = "+str(1-dist_middle_start)[:3])
+	print(middle_word+" --> "+middle_query+" similarity = "+str(1-dist_middle_middle)[:3])
+	print(middle_word+" --> "+end_query+" similarity = "+str(1-dist_middle_end)[:3])
+	print("=========================================")
+
 def astar_convene(start_query,end_query,encoder,weight=4.0,branching_factor=10,dictionary=None):
 	if dictionary is not None:
 		saved_start_query = start_query 
@@ -504,7 +644,7 @@ def ucs_algo(start_query,end_query,encoder,dictionary=None):
 		print(indent+item)
 	print("=========================================")
 
-def get_shortest_path(start_query,end_query,encoder,algo="UCS",dictionary=None):
+def get_shortest_path(start_query,end_query,encoder,algo="UCS",dictionary=None,middle_query=None):
 	default_b_factor = 15
 	default_weight = 4
 	sys.stdout.write("\nCalculating shortest vector from \""+str(start_query)+"\" to \""+str(end_query)+"\"...")
@@ -512,7 +652,7 @@ def get_shortest_path(start_query,end_query,encoder,algo="UCS",dictionary=None):
 	if algo == "UCS":
 		sys.stdout.write(" using UCS\n\n")
 		return ucs_algo(start_query,end_query,encoder,dictionary=dictionary)
-	elif algo in ["A*","C*"]:  
+	elif algo in ["A*","C*","3C*"]:  
 		sys.stdout.write(" using "+algo+"\n\n")
 		print("Note: high branching factor 	- less depth in final path")
 		print("Note: high A* weight 		- low cost but slower")
@@ -539,9 +679,29 @@ def get_shortest_path(start_query,end_query,encoder,algo="UCS",dictionary=None):
 				break
 			except: continue
 		print("\n")
+		if algo=="3C*": return astar_convene_3(start_query,middle_query,end_query,encoder,weight,b_factor)
 		if algo=="A*": return astar_path(start_query,end_query,encoder,weight=weight,branching_factor=b_factor,dictionary=dictionary)
 		if algo=="C*": return astar_convene(start_query,end_query,encoder,weight=weight,branching_factor=b_factor,dictionary=dictionary)
 	else: print("ERROR: algo input not recognized")
+
+def prep_3cstar(encoder):
+	while True:
+		query1 = raw_input("\nFirst query: ")
+		if query1 in ["exit","Exit"]: break
+		query2 = raw_input("Second query: ")
+		if query2 in ["exit","Exit"]: break
+		query3 = raw_input("Third query: ")
+		if query3 in ["exit","Exit"]: break
+		if query1==" " or query2==" " or query3==" ": continue
+		query1 = query1.replace(" ","_")
+		query1 = query1.lower()
+		query2 = query2.replace(" ","_")
+		query2 = query2.lower()
+		query3 = query3.replace(" ","_")
+		query3 = query3.lower()
+		dictionary = None 
+		get_shortest_path(query1,query2,encoder,algo="3C*",middle_query=query3)
+		print("\n")
 
 def path_search_interface():
 
@@ -555,7 +715,7 @@ def path_search_interface():
 	link_encoder = get_encoder('links.tsv',False,encoder_directory+'/links',400,500,1,5,20)
 
 	while True:
-		algo = raw_input("\nWhich algorithm (UCS, [A*], or C*): ")
+		algo = raw_input("\nWhich algorithm (UCS, [A*], C*, or 3C*): ")
 		if algo in ["ucs","UCS"]: 
 			algo = "UCS"
 			break
@@ -565,6 +725,8 @@ def path_search_interface():
 		if algo in ["convene","c","c*","C*"]:
 			algo = "C*"
 			break
+		if algo in ["3c","3C","3C*","3c*","3"]:
+			return prep_3cstar(text_encoder)
 
 		if algo in ["exit","Exit"]:
 			return
