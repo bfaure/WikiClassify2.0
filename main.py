ca5983d2e069348c4e89b8e862e15a72a6ded81b
Nathan Kjer <nathan.kjer@rutgers.edu>
2017-03-15 13:30:27 -0400

    Squashed bug

--- main.py
+++ main.py
@@ -316,21 +316,21 @@ def get_queries(text_encoder, category_encoder, link_encoder, n=None):
             queries = [q.lower() for q in queries]
             for q in queries:
                 if q not in text_encoder.model.index2word:
-                    print('%s not found! %' % q)
+                    print('%s not found!' % q)
                     return None, None
             return queries, text_encoder
         elif source.lower() in ['t']:
             queries = [q[0].upper()+q[1:] for q in queries]
             for q in queries:
                 if q not in text_encoder.model.index2word:
-                    print('%s not found! %' % q)
+                    print('%s not found!' % q)
                     return None, None
             return queries, link_encoder
         elif source.lower() in ['c']:
             queries = ['Category:'+q[0].upper()+q[1:] for q in queries]
             for q in queries:
                 if q not in text_encoder.model.index2word:
-                    print('%s not found! %' % q)
+                    print('%s not found!' % q)
                     return None, None
             return queries, category_encoder
 
