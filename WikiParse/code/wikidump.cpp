f0e4e20654d95ee77390a4f36d3b2d445a472952
bfaure <bfaure23@gmail.com>
2017-03-21 21:21:53 -0400

    small changes

--- WikiParse/code/wikidump.cpp
+++ WikiParse/code/wikidump.cpp
@@ -79,7 +79,7 @@ wikidump::wikidump(string &path, string &cutoff_date, string password) {
     server_dbname   = "ebdb";
 
     //server_capacity = 10000000000 // 10 GB
-    server_capacity  = 500000; // 50 KB
+    server_capacity  = 50000; // 10 KB
     //server_capacity = 100000000; // 100 MB
     replace_server_duplicates   = false; // dont replace duplicates
     server_write_buffer_size    = 10000; // write to server after this many read
