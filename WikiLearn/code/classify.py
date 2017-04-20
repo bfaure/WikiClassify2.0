143e0e6059add5e13bd2d53466bfe3978ba11258
Nathan Kjer <nathan.kjer@rutgers.edu>
2017-04-20 14:06:27 -0400

    Update classify.py

--- WikiLearn/code/classify.py
+++ WikiLearn/code/classify.py
@@ -62,3 +62,39 @@ class vector_classifier(object):
 
     def get_classes(self, matrix):
         return np.array([self.get_class(np.expand_dims(x,axis=0)) for x in matrix])
+    
+def plot_confusion_matrix(y_test, y_pred, class_names, normalize=False):
+    """
+    *** ONLY WORKS FOR NON-MULTICLASS ***
+    
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+
+    title='Confusion matrix'
+    cmap=plt.cm.Blues
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_test, y_pred)
+    
+    # Plot normalized confusion matrix
+    plt.figure()
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(class_names))
+    plt.xticks(tick_marks, class_names, rotation=45)
+    plt.yticks(tick_marks, class_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j,i,cm[i, j],horizontalalignment="center",color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
